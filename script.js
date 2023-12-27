ab0f3a1fb30ace63126c256465a44f0c0dc1f0a0
Ajasa Taiwo <ajasataiwo45@gmail.com>
2023-12-27 19:13:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const bttn = document.querySelector('#btn'); 
+const catUrl = 'https://catfact.ninja/fact'; 
+
+bttn.addEventListener('click', function() {
+    
+    // pick anyone to use 
+
+
+    handleFunction(); 
+    // handlemeasync(); 
+    
+}); 
+
+function handleFunction() {
+    // when this btn is clicked the we are going to send a request to an api and get data frome the api to 
+    // display is the white text box
+
+    // send request in js by using the fetch(); 
+   fetch(catUrl)
+    .then((response) => response.json())
+    .then((res) => { 
+        // console.log(res["fact"]);
+        const txt = document.querySelector(".txt"); 
+        txt.textContent = res["fact"]; 
+    })
+    .catch((err) => { 
+        console.error(err);
+    })
+
+
+}
+
+async function handlemeasync() { 
+    // give us the new keyword await that wait for the async funciton to resolve
+
+    const res = await fetch(catUrl, {
+        method:"GET",
+        headers: { 
+            "Content-type":"application/json"
+        }, 
+        // body: { something}
+    }); 
+
+    const format = await res.json(); 
+    const text = format["fact"]; 
+    const txt = document.querySelector(".txt"); 
+    txt.textContent = text; 
+    // console.log(text);
+
+
+}
